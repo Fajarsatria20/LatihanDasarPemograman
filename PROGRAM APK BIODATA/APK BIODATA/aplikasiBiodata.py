3792568815d3765fb02dbebd620b8a75b8a2abda
Fajarsatria20 <114629590+Fajarsatria20@users.noreply.github.com>
2022-09-29 11:31:33 +0700

    Add files via upload

---
+++ PROGRAM APK BIODATA/APK BIODATA/aplikasiBiodata.py
@@ -0,0 +1,14 @@
+#aplikasi biodata buatan create by fajar
+
+# memasukan nama
+print("Yuk Lengkapi data diri . .")
+
+#input data diri
+nama = input ("Nama                 : ")
+kelas = input ("kelas               : ")
+prodi = input ("prodi               : ")
+
+# print / output nama
+print("Nama anda adalah", Nama)
+print("Kelas anda adalah", Kelas)
+print("Prodi anda adalah", Prodi)
