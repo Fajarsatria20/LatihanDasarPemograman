652dc359f89c5fca50d2ad8ead6d9e7e7bb01981
Fajarsatria20 <114629590+Fajarsatria20@users.noreply.github.com>
2022-10-06 09:20:03 +0700

    Add files via upload

---
+++ tugas_sesi3.py
@@ -0,0 +1,7 @@
+# Muhammad Fajar Satria Adam
+nama = "Muhammad Fajar Satria Adam"
+kelas = "TI22A"
+jurusan = "Teknik Informatika"
+quote = "Your Only Limit is Your mind"
+
+print (nama, kelas, jurusan, quote)
